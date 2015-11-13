67c1891ebb7339df679a0409eeeda61921d1cdd6
daulet <daucloudlab@gmail.com>
2015-11-13 09:04:28 +0600

    Getting Started With Hibernate Annotations

---
+++ src/main/java/kz/kazntu/lessons/manager/PersonManager.java
@@ -0,0 +1,36 @@
+package kz.kazntu.lessons.manager;
+
+import kz.kazntu.lessons.models.Address;
+import kz.kazntu.lessons.models.Person;
+import kz.kazntu.lessons.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class PersonManager {
+
+    public static void main(String[] args) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+
+        Person steve = new Person("Steve", "Jobs");
+        Person donald = new Person("Donald", "Trump");
+
+        Address valley = new Address("Steve P Jobs", "San Francisco", "11111");
+        Address newyork = new Address("Trump Tower", "New York", "22222");
+        Address chicago = new Address("Trump Tower", "Chicago", "33333");
+
+        steve.getAddresses().add(valley);
+        donald.getAddresses().add(newyork);
+        donald.getAddresses().add(chicago);
+
+        System.out.println("Creating Person: " + steve.getFirstName());
+        session.persist(steve);
+        System.out.println("Creating Person: " + donald.getFirstName());
+        session.persist(donald);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+}
